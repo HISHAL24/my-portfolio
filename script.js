6ca33a96867fe1f4272d2c65409f368020b85ed7
Hishal MT <hishal@madeit.ac.in>
2026-05-24 17:43:43 +0530

    Fixed portfolio homepage

--- script.js
+++ script.js
@@ -435,3 +435,36 @@ window.addEventListener("load", () => {
     });
 
 });
+
+
+/* ========================================= */
+/* DARK MODE TOGGLE */
+/* ========================================= */
+
+const themeToggle = document.getElementById("themeToggle");
+
+themeToggle.addEventListener("click", () => {
+
+    document.body.classList.toggle("dark-mode");
+
+    // ICON CHANGE
+
+    const icon = themeToggle.querySelector("i");
+
+    if(document.body.classList.contains("dark-mode")){
+
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+
+        localStorage.setItem("theme","dark");
+
+    }else{
+
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+
+        localStorage.setItem("theme","light");
+    }
+
+});
+
